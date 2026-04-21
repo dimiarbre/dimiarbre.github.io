6f4173de7c0528bbe9a06b06fdad2a22f8899d7a
dimiarbre <56112141+dimiarbre@users.noreply.github.com>
2026-04-21 08:29:45 +0000

    Deploying to gh-pages from @ dimiarbre/dimiarbre.github.io@9edf22db238b32b525b93bdc208d4db754f9f1c3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -523,7 +523,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/cv_lereverend_dimitri.pdf", "_blank");
         },
       },{
         id: 'social-scholar',
